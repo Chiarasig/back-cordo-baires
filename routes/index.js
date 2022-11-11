d089f000df713d11f0e2e1d7e3ae5b78ee0e5401
MRwebdesign <mrwebdesign7@gmail.com>
2022-11-11 11:18:35 -0300

    created user MT-B1-16

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+let router = require('express').Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'back-cordo-baires' });
-});
+
+let user = require('./user')
+router.use('/user', user)
+
+
 
 module.exports = router;
