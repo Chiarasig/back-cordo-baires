473ab68ce63fbcc739717e8a4b9d84a718cb235f
MRwebdesign <mrwebdesign7@gmail.com>
2022-11-11 21:27:49 -0300

    Render cards and filter, Mt-01-19

--- routes/city.js
+++ routes/city.js
@@ -1,6 +1,8 @@
 let router = require('express').Router();
-let {create} = require('../controllers/city')
+let {create, read} = require('../controllers/city')
 
 router.route('/').post(create)
+//declarar el endpoint con el metodo que voy a usar, create, read, update, delete.
+router.route('/').get(read)
 
 module.exports = router;
