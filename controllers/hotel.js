bc69b2ea08ef37b1f08a73023cf292c9da2d929b
Chiarasig <chiarasignori37@gmail.com>
2022-11-12 18:40:01 -0300

    routes and controlers with filters

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -15,6 +15,41 @@ const controller = {
             message: error.message
         });
     }
+    },
+    read: async (req,res) => {
+        let {query} = req
+        let {order} = req
+        if (query.name){
+            query = {
+                ...query,
+                name: {$regex: query.name, $options: 'i'}
+            }
+        }
+        if (req.query.order){
+            order = {name: req.query.order}
+        }
+        try {
+            let all = await Hotel.find(query).sort(order);
+            if (all){
+                res.status(200).json({
+                response: all,
+                success: true,
+                message: "Hotels retrieved successfully"
+
+                })
+            } else {
+                res.status(404).json({
+                success: false,
+                message: "No hotels found"
+                })
+            }
+        } 
+        catch (error) {
+            res.status(400).json({
+                success: false,
+                message: error.message
+            })
+        }
 }}
 
 module.exports = controller;
