5dbbd699e6d1807b916507938ea568d7dadf6b01
Chiarasig <chiarasignori37@gmail.com>
2022-11-13 22:43:08 -0300

    route and controller to delete a show

--- routes/show.js
+++ routes/show.js
@@ -1,6 +1,8 @@
 let router = require('express').Router();
-let { create, update } = require('../controllers/show')
+let { create, update, destroy } = require('../controllers/show')
 
 router.route('/').post(create)
 router.route('/:id').patch(update)
+router.route('/:id').delete(destroy)
+
 module.exports = router
