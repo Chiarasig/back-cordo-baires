9d0b0acef314f094559df44fe10c427712352261
Chiarasig <chiarasignori37@gmail.com>
2022-11-14 22:27:52 -0300

    controllers and hotel and show detail routes

--- models/Hotel.js
+++ models/Hotel.js
@@ -4,8 +4,8 @@ const schema = new mongoose.Schema({
     name: {type: String, required: true},
     photo: [{type: String, required: true}],
     capacity: {type: Number, required: true},
-    cityId: [{type: mongoose.Types.ObjectId, ref: 'cities', required: true}],
-    userId: [{type: mongoose.Types.ObjectId, ref: 'admins', required: true}]
+    cityId: {type: mongoose.Types.ObjectId, ref: 'cities', required: true},
+    userId: {type: mongoose.Types.ObjectId, ref: 'users', required: true}
 })
 
 const Hotel = mongoose.model("hotels", schema)
--- routes/show.js
+++ routes/show.js
@@ -1,7 +1,8 @@
 let router = require('express').Router();
-let { create, update, destroy } = require('../controllers/show')
+let { create, update, destroy, read } = require('../controllers/show')
 
 router.route('/').post(create)
+router.route('/').get(read)
 router.route('/:id').patch(update)
 router.route('/:id').delete(destroy)
 
