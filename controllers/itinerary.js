2d4610e8a0d4a65a0434aa0d8267ab5165f70036
MRwebdesign <mrwebdesign7@gmail.com>
2022-11-14 23:21:33 -0300

    create itinerary, controller and router mt-b1-31

--- controllers/itinerary.js
+++ controllers/itinerary.js
@@ -1,43 +1,56 @@
-const Itinerary = require('../models/Itinerary');
-const Show = require('../models/Show');
+const Itinerary = require("../models/Itinerary");
 
 const controller = {
-    read: async (req,res) => {
-        let {query} = req
-        if (query.name){
-            query = {
-                ...query,
-                name: {$regex: query.name, $options: 'i'} //i es para que sea insensible a mayusculas y minusculas            
-            }
-        }
-        try{
-            let todos = await Itinerary.find(query).populate('cityId', 'name')
-            if(todos){
-                res.status(200).json({
-                    response: todos.map((item) => ({
-                      cityId: item.cityId,
-                      name: item.name,
-                      photo: item.photo,
-                      description: item.description,
-                      price: item.price,
-                      duration: item.duration,
-                    })),
-                    success: true,
-                    message: "show found",
-                  })    
-            }else{
-                res.status(404).json({
-                    success: false,
-                    message : "No cities found"
-                })
-            }
-        }
-        catch(err){
-            res.status(400).json({
-                success: false,
-                message: err.message
-            })
-        }
-}
-}
+  read: async (req, res) => {
+    let { query } = req;
+    if (query.name) {
+      query = {
+        ...query,
+        name: { $regex: query.name, $options: "i" }, //i es para que sea insensible a mayusculas y minusculas
+      };
+    }
+    try {
+      let todos = await Itinerary.find(query).populate("cityId", "name");
+      if (todos) {
+        res.status(200).json({
+          response: todos.map((item) => ({
+            cityId: item.cityId,
+            name: item.name,
+            photo: item.photo,
+            description: item.description,
+            price: item.price,
+            duration: item.duration,
+          })),
+          success: true,
+          message: "show found",
+        });
+      } else {
+        res.status(404).json({
+          success: false,
+          message: "No cities found",
+        });
+      }
+    } catch (err) {
+      res.status(400).json({
+        success: false,
+        message: err.message,
+      });
+    }
+  },
+  create: async (req, res) => {
+    try {
+      let new_itinerary = await Itinerary.create(req.body);
+      res.status(201).json({
+        id: new_itinerary.id,
+        success: true,
+        message: "Itinerary created successfully",
+      });
+    } catch (err) {
+      res.status(400).json({
+        success: false,
+        message: err.message,
+      });
+    }
+  },
+};
 module.exports = controller;
--- routes/itinerary.js
+++ routes/itinerary.js
@@ -1,6 +1,8 @@
 let router = require('express').Router();
-let {read} = require('../controllers/itinerary')
+let {read, create} = require('../controllers/itinerary');
+
 
 router.route('/').get(read)
+router.route('/').post(create)
 
 module.exports = router;
