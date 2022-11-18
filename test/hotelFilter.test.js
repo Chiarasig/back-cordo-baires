eb28763e7c307199a66a413a26a16b6677a6b7fb
Chiarasig <chiarasignori37@gmail.com>
2022-11-18 19:31:11 -0300

    check status 404

---
+++ test/hotelFilter.test.js
@@ -0,0 +1,24 @@
+const app = require('../app');
+const chai = require('chai');
+const expect = chai.expect;
+const request = require('supertest');
+
+
+describe('Check status 404', function(){
+    // it -test cases
+    it('Check status 404', function(done){
+
+        request(app)
+            .get('/api/hotel?name=city')
+            .end(function(err, res){
+                console.log(res.body)
+                expect(res.status).to.equal(404);
+                if(err){
+                    return done(err)
+                }
+                done()
+            })
+    })
+}) 
+
+
