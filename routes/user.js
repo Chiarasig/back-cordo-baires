8414b057a7b10daf860f9559f5b208d06abbd08a
Chiarasig <chiarasignori37@gmail.com>
2022-11-27 16:59:33 -0300

    controllers, rutes, middlewares and config to enter

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,18 @@
 let router = require('express').Router();
 const schema = require("../schemas/signUp")
+const schemaSignIn = require("../schemas/userSignIn")
 let validator = require('../middlewares/validator');
 const accountExistsSignUp = require("../middlewares/accountExistsSignUp")
-let {register, verified} = require('../controllers/user')
+const accountExistsSignIn = require('../middlewares/accountExistsSignIn')
+const accountHasBeenVerified = require('../middlewares/accountHasBeenVerified')
+const mustSignIn = require('../middlewares/mustSignIn')
+const passport = require('../config/passport')
+let {register, verified, enter, enterWithToken } = require('../controllers/user')
 
 router.post('/sign-up', validator(schema), accountExistsSignUp, register)
 router.get('/verify/:code', verified)
+router.post('/sign-in', validator(schemaSignIn),accountExistsSignIn, accountHasBeenVerified, enter)
+router.post('/token', passport.authenticate('jwt', { session:false }), mustSignIn, enterWithToken)
 
 
 module.exports = router;
