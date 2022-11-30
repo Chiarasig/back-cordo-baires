e36731c21fee8932491792ea006b1b392fc957b4
MRwebdesign <mrwebdesign7@gmail.com>
2022-11-30 13:54:21 -0300

    fix routers

--- routes/city.js
+++ routes/city.js
@@ -3,11 +3,13 @@ let {create, read, update, destroy} = require('../controllers/city')
 const validator = require('../middlewares/validator')
 const schema = require('../schemas/city')
 const passport = require('../config/passport')
+const isOwner = require('../middlewares/isOwner')
+const City = require('../models/City')
 
 router.route('/').get(read)
 router.route('/').post(passport.authenticate('jwt', { session:false }),validator(schema),create)
-router.route('/:id').put(passport.authenticate('jwt', { session:false }),update)
-router.route('/:id').delete(passport.authenticate('jwt', { session:false }),destroy)
+router.route('/:id').put(passport.authenticate('jwt', { session:false }),isOwner(City),update)
+router.route('/:id').delete(passport.authenticate('jwt', { session:false }),isOwner(City),destroy)
 //declarar el endpoint con el metodo que voy a usar, create, read, update, delete.
 
 module.exports = router;
--- routes/itinerary.js
+++ routes/itinerary.js
@@ -3,11 +3,13 @@ let {read, create, update, destroy} = require('../controllers/itinerary');
 const schema = require('../schemas/itinerary')
 const passport = require('../config/passport')
 const validator = require('../middlewares/validator')
+const isOwner = require('../middlewares/isOwner')
+const Itinerary = require('../models/Itinerary')
 
 
 router.route('/').get(read)
 router.route('/').post(passport.authenticate('jwt', { session:false }), validator(schema),create)
-router.route('/:id').put(passport.authenticate('jwt', { session:false }),update)
-router.route('/:id').delete(passport.authenticate('jwt', { session:false }),destroy)
+router.route('/:id').put(passport.authenticate('jwt', { session:false }),isOwner(Itinerary),update)
+router.route('/:id').delete(passport.authenticate('jwt', { session:false }),isOwner(Itinerary),destroy)
 
 module.exports = router;
