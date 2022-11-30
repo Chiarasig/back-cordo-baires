57fb33139237d6518a242e8cfbf43c75383ecdae
Chiarasig <chiarasignori37@gmail.com>
2022-11-30 13:09:54 -0300

    routes and controllers for comments

--- routes/hotel.js
+++ routes/hotel.js
@@ -2,12 +2,14 @@ let router = require('express').Router();
 const schema = require('../schemas/hotel')
 const validator = require('../middlewares/validator')
 const passport = require('../config/passport')
+const isOwner = require('../middlewares/isOwner')
+const Hotel = require('../models/Hotel')
 
 let { create, read, update, destroy } = require('../controllers/hotel')
 
 router.route('/').post(passport.authenticate('jwt', { session:false }),validator(schema),create)
 router.route('/').get(read)
-router.route('/:id').patch(passport.authenticate('jwt', { session:false }),update)
+router.route('/:id').patch(passport.authenticate('jwt', { session:false }), isOwner(Hotel),update)
 router.route('/:id').delete(passport.authenticate('jwt', { session:false }),destroy)
 
 module.exports = router
--- routes/show.js
+++ routes/show.js
@@ -3,11 +3,13 @@ const passport = require('../config/passport')
 const schema = require('../schemas/show')
 const validator = require('../middlewares/validator')
 let { create, update, destroy, read, readById } = require('../controllers/show')
+const isOwner = require('../middlewares/isOwner')
+const Show = require('../models/Show')
 
 router.route('/').post(passport.authenticate('jwt', { session:false }), validator(schema),create)
 router.route('/').get(read)
 router.route('/:id').get(readById)
-router.route('/:id').patch(passport.authenticate('jwt', { session:false }),update)
+router.route('/:id').patch(passport.authenticate('jwt', { session:false }), isOwner(Show),update)
 router.route('/:id').delete(passport.authenticate('jwt', { session:false }),destroy)
 
 module.exports = router
