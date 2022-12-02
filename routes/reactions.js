c5d87fcf63ab9a524ffc3a0c48e24ca7dd2f078c
MRwebdesign <mrwebdesign7@gmail.com>
2022-12-02 04:16:06 -0300

    mt-01-55 created reactions

--- routes/reactions.js
+++ routes/reactions.js
@@ -1,11 +1,16 @@
 let router = require('express').Router();
-let {create, update, read} = require('../controllers/reactions');
+let {create, update, read, reactionforuser, deletereactionofuser} = require('../controllers/reactions');
 const validator = require('../middlewares/validator');
 const schema = require('../schemas/reactions');
 const passport = require('../config/passport');
+const Reactions = require('../models/Reactions');
+const isOwnerReaction = require('../middlewares/isOwnerReaction');
 
 router.route('/').post(passport.authenticate('jwt', { session:false }),validator(schema),create)
 router.route('/').put(passport.authenticate('jwt', { session:false }),update)
 router.route('/').get(passport.authenticate('jwt', { session:false }),read)
+router.route('/reactionsforuser/').get(passport.authenticate('jwt', { session:false }),reactionforuser)
+router.route('/:id').put(passport.authenticate('jwt', { session:false }), isOwnerReaction(Reactions), deletereactionofuser)
+
 
 module.exports = router;
